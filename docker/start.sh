97d577e97906a0d1b11b51eceaad0b0f12051d52
mmuffins <mail@tiv73.com>
2023-02-08 22:47:26 +0100

    Update readme

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CRON_STRING="$CRON /usr/scheduler/publish/nzbhydra-schedule search -l $LOGLEVEL -s $SEARCHTERMSFILE -o $NZBOUTPUTDIRECTORY -u $NZBHYDRAURI -k $NZBHYDRAAPIKEY -a $MAXAGE -c $CATEGORY -i $INDEXERS -n $MINSIZE -m $MAXSIZE -d $APPENDRESOLUTION -q $REQUESTCOOLDOWN  > /proc/1/fd/1 2>/proc/1/fd/2\n"
+CRON_STRING="$CRON /usr/scheduler/publish/nzbhydra-schedule search -l $LOGLEVEL -s $SEARCHTERMSFILE -o $NZBOUTPUTDIRECTORY -u $NZBHYDRAURI -k $NZBHYDRAAPIKEY -a $MAXAGE -c $CATEGORY -i $INDEXERS -n $MINSIZE -m $MAXSIZE -q $REQUESTCOOLDOWN  > /proc/1/fd/1 2>/proc/1/fd/2\n"
 echo "Creating cron job $CRON_STRING"
 echo -e "$CRON_STRING" > crontab
 crontab crontab
