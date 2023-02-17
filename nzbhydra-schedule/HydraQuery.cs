c8d87fe1aab4889af94679cc4d181153f0502c0d
mmuffins <mail@tiv73.com>
2023-02-17 15:48:27 +0100

    Improve Download behavior

--- nzbhydra-schedule/HydraQuery.cs
+++ nzbhydra-schedule/HydraQuery.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,19 +31,19 @@ namespace nzbhydra_schedule
                 .ToList();
             Logger.WriteLog($"Parsed {Results.Count} results.", Logger.LogLevel.debug);
         }
+
+        private string? GetNzbFilename(HttpResponseMessage linkResponse)
+        {
+
+            var localPath = linkResponse.RequestMessage.RequestUri.LocalPath;
+            var pathArray = localPath.Split('/');
+            return pathArray[pathArray.Length - 1];
+        }
+
         public async Task SaveNzbAsync(HttpClient httpClient, DirectoryInfo directory)
         {
             foreach (var result in Results)
             {
-                var nzbPath = Path.Join(directory.FullName, result.GetNzbName());
-                Logger.WriteLog($"Downloading nzb to {nzbPath}.");
-
-                if (File.Exists(nzbPath))
-                {
-                    Logger.WriteLog($"{nzbPath} already exists, skpping download", Logger.LogLevel.warn);
-                    continue;
-                }
-
                 Logger.WriteLog($"Downloading from link {result.Link}.", Logger.LogLevel.debug);
                 HttpResponseMessage linkResponse;
 
@@ -52,7 +53,23 @@ namespace nzbhydra_schedule
                 }
                 catch (Exception ex)
                 {
-                    Logger.WriteLog($"An error occurred when downloading link {result.Link} for result {result.Title}.", Logger.LogLevel.err);
+                    Logger.WriteLog($"An error occurred when downloading link {result.Link} for result {result.Title}: {ex.Message}", Logger.LogLevel.err);
+                    continue;
+                }
+
+                var nzbFilename = GetNzbFilename(linkResponse);
+                if (nzbFilename == null )
+                {
+                    Logger.WriteLog($"Could not get filename from download.", Logger.LogLevel.err);
+                    continue;
+                }
+
+                var nzbPath = Path.Join(directory.FullName, nzbFilename);
+                Logger.WriteLog($"Downloading nzb to {nzbPath}.");
+
+                if (File.Exists(nzbPath))
+                {
+                    Logger.WriteLog($"{nzbPath} already exists, skpping download", Logger.LogLevel.warn);
                     continue;
                 }
 
